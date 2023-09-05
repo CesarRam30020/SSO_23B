a36487a2bbd6778377f4beaff14f6801a2cba15a
geravid <eric.garduno9132@alumnos.udg.mx>
2023-09-05 10:02:35 -0600

    Variables types and methods returns types

--- practica_2/Lote.py
+++ practica_2/Lote.py
@@ -1,86 +1,95 @@
 import re
 
-class Lote():
-	def __init__(self, numLote = 0, procesos = []):
-		self.__num = numLote
-		self.__procesos = []
-		self.__maxProcesos = 4
+MAX_PROCESOS = 4
+LOTE_INICIAL = 0
+TIEMPO_ESTIMADO_SEGUNDOS_INICIAL = 0
+NO_PROGRAMA_INICIAL = -1
 
-	def agregar(self,proceso):
-		valido = len(self.__procesos) < self.__maxProcesos
+
+class Lote():
+	def __init__(self, numLote: int = LOTE_INICIAL, procesos: list = []) -> None:
+		self._num : int = numLote
+		self._procesos : list = []
+		self._maxProcesos = MAX_PROCESOS
+
+	def agregar(self,proceso) -> bool:
+		valido = len(self._procesos) < self._maxProcesos
 		if valido:
-			self.__procesos.append(proceso)
+			self._procesos.append(proceso)
+		
 		return valido
 
-	def procesos(self):
-		return self.__procesos
+	def procesos(self) -> list:
+		return self._procesos
 
-	def fijaNum(self,num):
-		self.__num = num
+	def fijaNum(self,num) -> int:
+		self._num = num
 
 	def dameNum(self):
-		return self.__num
+		return self._num
 
-	def lleno(self):
-		return len(self.__procesos) >= self.__maxProcesos
+	def lleno(self) -> bool:
+		return len(self._procesos) >= self._maxProcesos
 
-	def __str__(self):
-		s = "Numero de Lote: "+str(self.__num)+"\n"
-		s += "Procesos: "+str(len(self.__procesos))+"\n"
+	def __str__(self) -> str:
+		s = "Numero de Lote: "+str(self._num)+"\n"
+		s += "Procesos: "+str(len(self._procesos))+"\n"
 		return s
 
+
 class Proceso():
-	def __init__(self,programador,operacion,tiempoMaximoEstimado,noPrograma):
+	def __init__(self,programador,operacion,tiempoMaximoEstimado,noPrograma) -> None:
 		self.__programador = ""
-		self.__operacion = "0/0"
-		self.__tiempoEstimadoSegundos = 0
-		self.__noPrograma = -1
+		self._operacion = "0/0"
+		self._tiempoEstimadoSegundos = TIEMPO_ESTIMADO_SEGUNDOS_INICIAL
+		self._noPrograma = NO_PROGRAMA_INICIAL
 
 		self.fijaProgramador(programador)
 		self.fijaOperacion(operacion)
 		self.fijaTiempoMaximoEstimado(tiempoMaximoEstimado)
 		self.fijaNoPrograma(noPrograma)
 
-	def fijaProgramador(self,programador):
+	def fijaProgramador(self,programador) -> bool:
 		valida = programador != ""
 		if valida:
 			self.__programador = programador
 		return valida
 
-	def fijaOperacion(self,operacion):
-		valida = bool(re.match(r"^[0-9]+\.?[0-9]*[+-/*%^][0-9]+\.?[0-9]*$",operacion))
+	def fijaOperacion(self,operacion) -> bool:
+		rgx = r"^[0-9]+\.?[0-9]*[+-/*%^][0-9]+\.?[0-9]*$"
+		valida = bool(re.match(rgx, operacion))
 		if valida:
-			self.__operacion = operacion
+			self._operacion = operacion
 		return valida
 
-	def fijaTiempoMaximoEstimado(self,tiempoMaximoEstimado):
+	def fijaTiempoMaximoEstimado(self,tiempoMaximoEstimado) -> bool:
 		valida = tiempoMaximoEstimado != "" and float(tiempoMaximoEstimado) > 0
 		if valida:
-			self.__tiempoEstimadoSegundos = float(tiempoMaximoEstimado)
+			self._tiempoEstimadoSegundos = float(tiempoMaximoEstimado)
 		return valida
 
-	def fijaNoPrograma(self,noPrograma):
+	def fijaNoPrograma(self,noPrograma) -> bool:
 		valida = noPrograma != "" and int(noPrograma) >= 0
 		if valida:
-			self.__noPrograma = int(noPrograma)
+			self._noPrograma = int(noPrograma)
 		return valida
 
-	def dameProgramador(self):
+	def dameProgramador(self) -> str:
 		return self.__programador
 
-	def dameOperacion(self):
-		return self.__operacion
+	def dameOperacion(self) -> str:
+		return self._operacion
 
-	def dameTiempoEstimadoSegundos(self):
-		return self.__tiempoEstimadoSegundos
+	def dameTiempoEstimadoSegundos(self) -> float:
+		return self._tiempoEstimadoSegundos
 
-	def dameNoPrograma(self):
-		return self.__noPrograma
+	def dameNoPrograma(self) -> int:
+		return self._noPrograma
 
 	def resolver(self):
 		pos = self.__posOperador()
-		operandos = [float(self.__operacion[:pos[0]]),float(self.__operacion[pos[1]:])]
-		operador = self.__operacion[pos[0]:pos[1]]
+		operandos = [float(self._operacion[:pos[0]]),float(self._operacion[pos[1]:])]
+		operador = self._operacion[pos[0]:pos[1]]
 		return self.__resolver(operandos,operador)
 
 	def __resolver(self,operandos,operador):
@@ -99,25 +108,25 @@ class Proceso():
 		return 0
 
 	def __posOperador(self):
-		pos = re.search(r"(\+|\-|/|\*|%|\^)",self.__operacion)
+		pos = re.search(r"(\+|\-|/|\*|%|\^)",self._operacion)
 		return pos.span()[0],pos.span()[1]
 
 	def valido(self):
 		errores = ""
 		if self.__programador == "" :
 			errores += "Nombre del Programador Invalido\n"
-		if self.__operacion == "0/0" or self.__operacion == "":
+		if self._operacion == "0/0" or self._operacion == "":
 			errores += "Operacion invalida\n"
-		if self.__tiempoEstimadoSegundos == 0:
+		if self._tiempoEstimadoSegundos == 0:
 			errores += "Tiempo Estimado Invalido, debe ser mayor a 0\n"
-		if self.__noPrograma == -1:
+		if self._noPrograma == -1:
 			errores += "Numero de Programa Invalido, ya existe"
 		return len(errores) == 0,errores
 
 	def __str__(self):
 		s = "Proceso:\n"
 		s += "Programador: "+self.__programador+"\n"
-		s += "Operacion: "+self.__operacion+"\n"
-		s += "Tiempo Maximo Estimado: "+str(self.__tiempoEstimadoSegundos)+"\n"
-		s += "# Programa: "+str(self.__noPrograma)+"\n"
+		s += "Operacion: "+self._operacion+"\n"
+		s += "Tiempo Maximo Estimado: "+str(self._tiempoEstimadoSegundos)+"\n"
+		s += "# Programa: "+str(self._noPrograma)+"\n"
 		return s
